b76042a1885a8725dbc9fc89faa27f5f94daaa32
Mishab <85479103+imishab@users.noreply.github.com>
2023-06-28 18:10:57 +0530

    v1.2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,48 +1,42 @@
 import React from 'react'
 
+
+
 const Home = () => {
-  return (
-    
-      <>
-      <div className="header-2">
 
-<nav className="bg-white py-2 md:py-4">
-  <div className="container px-4 mx-auto md:flex md:items-center">
+  return ( <>
 
-    <div className="flex justify-between items-center">
-      <a href="#" className="font-bold text-xl text-indigo-600">VectorCrop.</a>
-      <button className="border border-solid border-gray-600 px-3 py-1 rounded text-gray-600 opacity-50 hover:opacity-75 md:hidden" id="navbar-toggle">
-        <i className="fas fa-bars"></i>
+<div className="bg-black">
+  <div className="container mx-auto flex justify-between items-center py-3">
+  <div className="w-full flex flex-col py-4">
+      <div className="flex items-center justify-center my-1">
+        <span className="cursor-pointer font-semibold	 text-3xl text-white">
+          VectorCrop
+        </span>
+      </div>
+      </div>
+    <div className="flex space-x-6">
+      <button className="text-white">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" id="search"><path d="M11 22C4.935 22 0 17.065 0 11S4.935 0 11 0s11 4.935 11 11-4.935 11-11 11zm0-20c-4.962 0-9 4.037-9 9s4.038 9 9 9 9-4.037 9-9-4.038-9-9-9z"></path><path d="M23 24a.997.997 0 0 1-.707-.293l-4.795-4.795a1 1 0 0 1 1.415-1.414l4.794 4.795A.999.999 0 0 1 23 24z"></path></svg>
+      </button>
+      <button className="text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
+      </button>
+      <button className="text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
       </button>
     </div>
-
-    <div className="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
-      <a href="/Home" className="p-2 lg:px-4 md:mx-2 text-indigo-600">Home</a>
-      <a href="#" className="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300">About</a>
-      <a href="#" className="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300">Features</a>
-      <a href="#" className="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300">Pricing</a>
-      <a href="#" className="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300">Contact</a>
-      <a href="/login" className="p-2 lg:px-4 md:mx-2 text-indigo-600 text-center border border-transparent rounded hover:bg-indigo-100 hover:text-indigo-700 transition-colors duration-300">Login</a>
-      <a href="/signup" className="p-2 lg:px-4 md:mx-2 text-indigo-600 text-center border border-solid border-indigo-600 rounded hover:bg-indigo-600 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">Signup</a>
-    </div>
-  </div>
-</nav>
-
-<div className="bg-indigo-100 py-12 md:py-12">
-  <div className="container px-4 mx-auto">
-
-    <div className="text-center max-w-2xl mx-auto">
-      <h1 className="text-3xl md:text-4xl font-medium mb-2">Get the header you needed for your awesome website.</h1>
-      <button className="bg-indigo-600 text-white py-2 px-6 rounded-full text-xl mt-6">Get Started</button>
-      
-    </div>
   </div>
 </div>
-</div>
-      
-   </>
 
-  )
+
+
+      
+</> )
 }
 
 export default Home
